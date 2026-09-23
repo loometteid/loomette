60b0c3f68026be60c2035bf277922bce0da7c516
Hokki Suwanda <hokkyss2@gmail.com>
2026-09-23 15:10:18 +0700

    fix: expire cache

--- components/features/calendar/server-functions/revalidate-calendar-cache.server-function.ts
+++ components/features/calendar/server-functions/revalidate-calendar-cache.server-function.ts
@@ -1,8 +1,7 @@
 'use server';
 
-import { revalidatePath, updateTag } from "next/cache";
+import { updateTag } from "next/cache";
 
 export default async function revalidateCalendarCache(userId: string): Promise<void> {
-    updateTag(`diary-${userId}`);
-    revalidatePath("/calendar");
+    updateTag(`diary-${userId}`)
 }
